fd355359d25ecdd1b8d3c542ec3dde396b89767c
Ilyas BOUTEBAL <ilbou@valaa.dhcp.lvl.intranet>
2013-06-12 10:09:15 +0200

    creating block for Pfay_Test module

---
+++ app/code/local/Pfay/Test/Block/Monblock.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * exemple d'un block pour le module Pfay_Test
+ */
+
+class Pfay_Test_Block_Monblock extends Mage_Core_Block_Template
+{
+	
+	public function methodblock()
+	{
+		return 'informations de mon block Test';
+	}
+}
+?>
--- app/code/local/Pfay/Test/controllers/IndexController.php
+++ app/code/local/Pfay/Test/controllers/IndexController.php
@@ -11,7 +11,8 @@ class Pfay_Test_IndexController extends Mage_Core_Controller_Front_Action
 	
 	public function indexAction()
 	{
-		echo 'Test Index';
+		$this->loadLayout();
+		$this->renderLayout();
 		
 	}
 	
