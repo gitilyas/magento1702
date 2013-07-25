9cac0f439deb2ae0d887e8482f72ef184e0bf45d
Ilyas BOUTEBAL <ilbou@lotus.dhcp.lvl.intranet>
2013-07-25 15:22:26 +0200

    installation

--- README.md
+++
@@ -1,4 +0,0 @@
-magento1702
-===========
-
-magento 1.7.0.2 e-commerce framework
--- app/code/local/Pfay/Test/controllers/Checkout/CartController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "Mage/Checkout/controllers/CartController.php";
-class Pfay_Test_Checkout_CartController extends Mage_Checkout_CartController
-{
-    # surcharge de indexAction
-    public function indexAction() {
-        die('votre mothode a bien été surchargé !!');
-    }
-}
